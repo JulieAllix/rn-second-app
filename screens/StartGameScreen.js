513488678a382106df5ce11fa16e7cec4c3d992c
JulieAllix <allixjulie@gmail.com>
2020-06-08 14:28:57 +0200

    Reset function set

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,26 +1,63 @@
-import React from 'react';
-import { View, Text, StyleSheet, TextInput, Button, TouchableOpacity } from 'react-native';
+import React, { useState } from 'react';
+import { 
+    View, 
+    Text, 
+    StyleSheet, 
+    Button, 
+    TouchableOpacity,
+    TouchableWithoutFeedback,
+    Keyboard
+ } from 'react-native';
 
 import Card from '../components/Card';
+import Input from '../components/Input';
 import Colors from '../constants/themeColors';
 
 const StartGameScreen = props => {
+
+    const [enteredValue, setEnteredValue] = useState('');
+
+    const numberInputHandler = inputText => {
+        // replace anything that's not a number from 0 to 9, globally, with an empty string
+        setEnteredValue(inputText.replace(/[^0-9]/g, ''));
+    };
+
+    const resetInputHandler = () => {
+        setEnteredValue('');
+    };
+
     return (
-        <View style={styles.screen}>
-            <Text style={styles.title}>Start a New Game !</Text>
-            <Card style={styles.inputContainer}>
-                <Text style={styles.subTitle}>Select a Number</Text>
-                <TextInput />
-                <View style={styles.buttonsContainer}>
-                    <TouchableOpacity style={styles.button} onPress={() => {}}>
-                        <Text style={styles.text}>Reset</Text>
-                    </TouchableOpacity>
-                    <TouchableOpacity style={styles.button} onPress={() => {}}>
-                        <Text style={styles.text}>Confirm</Text>
-                    </TouchableOpacity>
-                </View>
-            </Card>
-        </View>
+        // The keyboard disappears when the user taps anywhere on the screen
+        <TouchableWithoutFeedback 
+            onPress={() => {
+                Keyboard.dismiss();
+            }}
+        >
+            <View style={styles.screen}>
+                <Text style={styles.title}>Start a New Game !</Text>
+                <Card style={styles.inputContainer}>
+                    <Text style={styles.subTitle}>Select a Number</Text>
+                    <Input 
+                        style={styles.input} 
+                        blurOnSubmit 
+                        autoCapitalize='none' 
+                        autoCorrect={false} 
+                        keyboardType='number-pad'
+                        maxLength={2} 
+                        onChangeText={numberInputHandler}
+                        value={enteredValue}
+                    />
+                    <View style={styles.buttonsContainer}>
+                        <TouchableOpacity style={styles.button} onPress={resetInputHandler}>
+                            <Text style={styles.text}>Reset</Text>
+                        </TouchableOpacity>
+                        <TouchableOpacity style={styles.button} onPress={() => {}}>
+                            <Text style={styles.text}>Confirm</Text>
+                        </TouchableOpacity>
+                    </View>
+                </Card>
+            </View>
+        </TouchableWithoutFeedback>
     );
 };
 
@@ -33,7 +70,7 @@ const styles = StyleSheet.create({
     title: {
         fontSize: 20,
         marginVertical: 10,
-        color: Colors.primary
+        color: Colors.primary,
     },
     subTitle: {
         fontSize: 15,
@@ -60,7 +97,11 @@ const styles = StyleSheet.create({
     },
     text: {
         color: Colors.light,
-        fontWeight: 'bold'
+        fontWeight: 'bold',
+    },
+    input: {
+        width: 50,
+        textAlign: 'center',
     }
 });
 
