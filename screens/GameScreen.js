76d715e0951f88ecb7921a85a43a14aef88f3720
JulieAllix <allixjulie@gmail.com>
2020-06-15 11:46:28 +0200

    Change game layout according to orientation

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -41,10 +41,25 @@ const GameScreen = props => {
     const initialGuess = generateRandomBetween(1, 100, props.userChoice);
     const [currentGuess, setCurrentGuess] = useState(initialGuess);
     const [pastGuesses, setPastGuesses] = useState([initialGuess.toString()]);
+    const [availableDeviceWidth, setAvailableDeviceWidth] = useState(Dimensions.get('window').width);
+    const [availableDeviceHeight, setAvailableDeviceHeight] = useState(Dimensions.get('window').height);
     const currentLow = useRef(1);
     const currentHigh = useRef(100);
 
     const { userChoice, onGameOver } = props;
+
+    useEffect(() => {
+        const updateLayout = () => {
+            setAvailableDeviceWidth(Dimensions.get('window').width);
+            setAvailableDeviceHeight(Dimensions.get('window').height);
+        };
+
+        Dimensions.addEventListener('change', updateLayout);
+
+        return () => {
+            Dimensions.removeEventListener('change', updateLayout);
+        };
+    });
 
     useEffect(() => {
         if (currentGuess === props.userChoice) {
@@ -69,6 +84,41 @@ const GameScreen = props => {
         setPastGuesses(curPastGuesses => [nextNumber.toString(), ...curPastGuesses]);
     };
 
+    let listContainerStyle = styles.listContainer;
+
+    if (availableDeviceWidth < 350) {
+        listContainerStyle = styles.listContainerBig;
+    };
+
+    if (availableDeviceHeight < 500) {
+        return (
+            <View style={styles.screen}>
+                <TitleText style={styles.title}>Opponent's Guess</TitleText>
+                <View style={styles.controls}>
+                    <MainButton onPress={nextGuessHandler.bind(this, 'lower')}>
+                        <Ionicons name="md-remove" size={24} />
+                    </MainButton>
+                    <NumberContainer>{currentGuess}</NumberContainer>
+                    <MainButton onPress={nextGuessHandler.bind(this, 'greater')}>
+                        <Ionicons name="md-add" size={24} />
+                    </MainButton>
+                </View>
+                <View style={listContainerStyle}>
+                    
+                    {/*</View><ScrollView contentContainerStyle={styles.list}>
+                        {pastGuesses.map((guess, index) => renderListItem(guess, pastGuesses.length - index))}
+                    </ScrollView>*/}
+                    <FlatList 
+                        keyExtractor={item => item} 
+                        data={pastGuesses} 
+                        renderItem={renderListItem.bind(this, pastGuesses.length)}
+                        contentContainerStyle={styles.list}
+                    />
+                </View>
+            </View>
+        );
+    }
+
     return (
         <View style={styles.screen}>
             <TitleText style={styles.title}>Opponent's Guess</TitleText>
@@ -81,7 +131,7 @@ const GameScreen = props => {
                     <Ionicons name="md-add" size={24} />
                 </MainButton>
             </Card>
-            <View style={styles.listContainer}>
+            <View style={listContainerStyle}>
                 
                 {/*</View><ScrollView contentContainerStyle={styles.list}>
                     {pastGuesses.map((guess, index) => renderListItem(guess, pastGuesses.length - index))}
@@ -116,8 +166,19 @@ const styles = StyleSheet.create({
         paddingHorizontal: 25,
         marginVertical: Dimensions.get('window').height > 600 ? 20 : 10,
     },
+    controls: {
+        flexDirection: 'row',
+        justifyContent: 'space-around',
+        alignItems: 'center',
+        width: '80%',
+    },
     listContainer: {
-        width: Dimensions.get('window').width > 350 ? '60%' : '80%',
+        //width: Dimensions.get('window').width > 350 ? '60%' : '80%',
+        width: '80%',
+        flex: 1,
+    },
+    listContainerBig: {
+        width: '60%',
         flex: 1,
     },
     list: {
