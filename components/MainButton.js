9531854ce2f1a1775d02a4c446e7985c1b7bccef
JulieAllix <allixjulie@gmail.com>
2020-06-15 14:45:52 +0200

    Change component according to device

--- components/MainButton.js
+++ components/MainButton.js
@@ -1,5 +1,13 @@
 import React, { useState, useEffect } from 'react';
-import { View, Text, StyleSheet, TouchableOpacity, Dimensions } from 'react-native';
+import { 
+    View, 
+    Text, 
+    StyleSheet, 
+    TouchableOpacity, 
+    Dimensions, 
+    TouchableNativeFeedback,
+    Platform
+} from 'react-native';
 
 import Colors from '../constants/themeColors';
 import BodyText from '../components/BodyText';
@@ -7,6 +15,12 @@ import BodyText from '../components/BodyText';
 const MainButton = props => {
     const [buttonWidth, setButtonWidth] = useState(Dimensions.get('window').width / 4);
 
+    let ButtonComponent = TouchableOpacity;
+/*
+    if(Platform.OS === 'android' && Platform.Version >= 21) {
+        ButtonComponent = TouchableNativeFeedback;
+    };
+*/
     useEffect(() => {
         const updateLayout = () => {
             setButtonWidth(Dimensions.get('window').width / 4);
@@ -20,11 +34,11 @@ const MainButton = props => {
     });
 
     return (
-        <TouchableOpacity style={{...styles.button, width:buttonWidth}} onPress={props.onPress}>
+        <ButtonComponent style={{...styles.button, width:buttonWidth}} onPress={props.onPress}>
             <BodyText style={styles.text}>
                 {props.children}
             </BodyText>
-        </TouchableOpacity>
+        </ButtonComponent>
     )
 };
 
